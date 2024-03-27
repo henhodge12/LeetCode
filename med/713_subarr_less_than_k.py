2784a40614b95e2a0744adc193422828319b1e63
Henry Hodge <henryhodge@Henrys-MBP-7.attlocal.net>
2024-03-26 23:44:32 -0400

    new problem

--- med/713_subarr_less_than_k.py
+++ med/713_subarr_less_than_k.py
@@ -1,24 +1,26 @@
+# 3/26/2024
+# i had to look at the solution for this because mine was too slow
+# it uses the 'sliding window' technique, where we increment the left
+# and right indices with each iteration, constaining our time complexity to O(n)
+# i used nested for loops first which is clearly O(n^2)
 class Solution(object):
     def numSubarrayProductLessThanK(self, nums, k):
-        totalscore = 0
-        totalarray = 0
-        for i in range(0, len(nums)):
-            totalscore = nums[i]
-            if totalscore < k:
-                totalarray += 1
-            else:
-                continue
-            for j in range(i+1, len(nums)):
-                totalscore += nums[j]
-                if totalscore >= k:
-                    break
-                else:
-                    totalarray += 1
-        return totalscore
-
+        product = 1
+        r = 0
+        l = 0
+        total = 0
+        if k <= 1:
+            return 0
+        while r < len(nums):
+            product *= nums[r]
+            while product >= k:
+                product /= nums[l]
+                l += 1
+            total += 1 + (r - l)
+            r += 1
+        return total
         """
         :type nums: List[int]
         :type k: int
         :rtype: int
         """
-
