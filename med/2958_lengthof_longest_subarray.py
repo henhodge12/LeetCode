3d560c5befe3ff8b211a51cdd6399af7198e3dc6
Henry Hodge <henryhodge@Henrys-MBP-7.attlocal.net>
2024-03-28 01:44:04 -0400

    new problem

---
+++ med/2958_lengthof_longest_subarray.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maxSubarrayLength(self, nums, k):
+        r, l, max = 0, 0, 0
+
+        n = len(nums)
+        while r < n:
+
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
